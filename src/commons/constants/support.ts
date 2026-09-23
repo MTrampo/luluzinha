b1d0260dcdaddff989a13f7bfb329ab67e84b287
vinicius <vinnicius4@hotmail.com>
2026-09-23 17:54:25 -0300

    fix: ajuste de função q retorna o erro das envs

--- src/commons/constants/support.ts
+++ src/commons/constants/support.ts
@@ -1,5 +1,4 @@
-import { getRequiredEnv } from "./env";
-
-export const whatsappNumber = getRequiredEnv("NEXT_PUBLIC_SUPPORT_WHATSAPP");
+export const whatsappNumber = process.env.NEXT_PUBLIC_SUPPORT_WHATSAPP || "";
 export const SUPPORT_WHATSAPP_NUMBER = whatsappNumber;
+
 
